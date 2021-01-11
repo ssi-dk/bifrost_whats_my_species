a64c88f09f53d7b508879e2720b8ff8175cdeaa3
Kim Ng <kimleeng@gmail.com>
2021-01-11 14:37:10 +0100

    forgot to save

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="kimn@ssi.dk",
     packages=find_packages(),
     install_requires=[
-        'bifrostlib >= 2.1.2',
+        'bifrostlib >= 2.1.4',
     ],
     package_data={"bifrost_whats_my_species": ['config.yaml', 'pipeline.smk']},
     include_package_data=True
